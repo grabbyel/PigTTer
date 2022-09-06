cef203de54f12949970d2286a6eee9132274e1c6
sergeisg <sergeisobera@gmail.com>
2022-09-06 20:19:14 +0200

    arreglado url de users.js

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -2,11 +2,7 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
-usersRouter.get('/', (request, response) => {
-    response.send('<h3>Fake Twitter Backend - Users</h3>')
-})
-
-usersRouter.get('/api/users', async (request, response) => {
+usersRouter.get('/', async (request, response) => {
     const users = await User.find({})
     response.json(users)
 })
