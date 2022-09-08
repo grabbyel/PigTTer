12c974cee5ddadea11343351096ca4a9bb52c3e3
Antonio Piñero <anpisan@gmail.com>
2022-09-08 15:02:34 +0200

    cambios hasta las 15.00 dia 08/09

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import SidebarComponent from "./components/container/SidebarComponent"
 import "./App.css"
 import FeedComponent from "./components/container/FeedComponent"
 import {useState} from 'react'
+import AuthScreenComponent from "./pages/Auth/AuthScreenComponent";
 
 
 function App() {
@@ -9,16 +10,19 @@ function App() {
 const [user, setUser] = useState(null);
     
     return (
-        <div className="container">
-            <div className="row">
-                <div className="col-md-4 col-sm-5 col-4">
-                    <SidebarComponent />
-                </div>
-                <div className="col-md-8 col-sm-7 col-8">
-                    <FeedComponent setUser={setUser} />
-                    {user && <h2>{user.username}</h2>}
-                </div>
-            </div>
+        // <div className="container">
+        //     <div className="row">
+        //         <div className="col-md-4 col-sm-5 col-4">
+        //             <SidebarComponent />
+        //         </div>
+        //         <div className="col-md-8 col-sm-7 col-8">
+        //             <FeedComponent setUser={setUser} />
+        //             {user && <h2>{user.username}</h2>}
+        //         </div>
+        //     </div>
+        // </div>
+        <div>
+            <AuthScreenComponent/>
         </div>
     )
 }
--- frontend/src/pages/Auth/AuthScreenComponent.jsx
+++ frontend/src/pages/Auth/AuthScreenComponent.jsx
@@ -1,13 +1,40 @@
-import React from 'react'
+import React from "react";
+import logo from "../../assets/logo_blanco_cerdo.png"
 
 const AuthScreenComponent = () => {
-  return (
-      <div>
-        componente pantalla de registro y login
-            
-        
-      </div>
-  )
-}
+    return (
+        <div className="background">
+            <div>
+                <h1>PIGTTER</h1>
+            </div>
+            <div>
+                <img src={logo}
+                    alt="logo"/>
+            </div>
 
-export default AuthScreenComponent
+            <div className="divBotones">
+                <button className="btn btn-primary  mt-5 rounded-5 fs-4" variant="primary">
+                    Iniciar Sesión
+                </button>
+                <button className="btn btn-primary  mt-5 rounded-5 fs-4" variant="primary">
+                    Registrarse
+                </button>
+            </div>
+            {/* <div className="titulo">
+                <span style={{--start-color: #007cf0; --end-color: #00dfd8; --content: '¿Listo...'}}>
+                    LISTO...
+                </span>
+                <span style="--start-color: #7928ca; --end-color: #ff0080; --content: 'Preview.'; --animation: a2">
+                    PARA...
+                </span>
+                <span style="--start-color: #ff4d4d; animation-name: a3; --end-color: #f9cb28; --content: 'Ship.'; --animation: a3">
+                    PIGTEAR ?
+                </span>
+            </div> */}
+        </div>
+
+
+    );
+};
+
+export default AuthScreenComponent;
