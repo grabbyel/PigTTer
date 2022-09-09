a0884d0b9b88939bc391bf2b28a3ecb981b4b2ec
sergeisg <sergeisobera@gmail.com>
2022-09-09 10:04:50 +0200

    push para debug

--- frontend/src/components/container/PigComponent.jsx
+++ frontend/src/components/container/PigComponent.jsx
@@ -6,13 +6,32 @@ import logo from '../../assets/logo_blanco_cerdo.png'
 
 function PigComponent() {
   const [show, setShow] = useState(false);
+  const [tweetContent, setTweetContent] = useState('');
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
+  const handleTextChange = (e) => {
+    e.preventDefault()
+    setTweetContent(e.target.value)
+    console.log('hola handle')
+    console.log(tweetContent)
+  }
+
+  const tweetear = (e) => {
+    e.preventDefault()
+    console.log('hola tweetear')
+    console.log(tweetContent)
+    handleClose()
+  }
+
   return (
     <>
-      <Button className='btn btn-primary w-75 mt-5 rounded-5 fs-4' variant="primary" onClick={handleShow}>
+    <form onSubmit={tweetear}>
+      <input/>
+      <button></button>
+    </form>
+      {/* <Button className='btn btn-primary w-75 mt-5 rounded-5 fs-4' variant="primary" onClick={handleShow}>
         <span className="fs-4 ms-1 d-none d-md-inline w-fit-content" style={{color: 'white'}}>Pigttear</span> 
         <span className="d-md-none"><img width="50" src={logo} alt="cerdo" /> </span>
       </Button>
@@ -22,13 +41,17 @@ function PigComponent() {
           <Modal.Title>¿Que te ocurre, criatura?</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form>
+          <Form onSubmit={tweetear}>
             <Form.Group
               className="mb-3"
               controlId="exampleForm.ControlTextarea1"
+              
             >
               <Form.Label>¡Pigttea aquí!</Form.Label>
-              <Form.Control as="textarea" rows={3} />
+              <Form.Control
+              onChange={handleTextChange}
+              value={tweetContent} 
+              />
             </Form.Group>
           </Form>
         </Modal.Body>
@@ -36,11 +59,11 @@ function PigComponent() {
           <Button className='rounded-5' variant="secondary" onClick={handleClose}>
             No Pigttear
           </Button>
-          <Button className='rounded-5' variant="primary" onClick={handleClose}>
+          <Button className='rounded-5' variant="primary" type="submit">
             Pigttear
           </Button>
         </Modal.Footer>
-      </Modal>
+      </Modal> */}
     </>
   );
 }
