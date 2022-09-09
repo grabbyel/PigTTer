4970e89b93534c1eae5fdab510e370d8be0b4786
Cristina Garcia <gscristina112@gmail.com>
2022-09-09 10:13:38 +0200

    `pigcomponent

--- frontend/src/components/container/PigComponent.jsx
+++ frontend/src/components/container/PigComponent.jsx
@@ -1,39 +1,50 @@
-import React, { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import Modal from 'react-bootstrap/Modal';
-import logo from '../../assets/logo_blanco_cerdo.png'
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Modal from "react-bootstrap/Modal";
+import logo from "../../assets/logo_blanco_cerdo.png";
 
 function PigComponent() {
   const [show, setShow] = useState(false);
-  const [tweetContent, setTweetContent] = useState('');
+  const [tweetContent, setTweetContent] = useState("");
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
   const handleTextChange = (e) => {
-    e.preventDefault()
-    setTweetContent(e.target.value)
-    console.log('hola handle')
-    console.log(tweetContent)
-  }
+    e.preventDefault();
+    setTweetContent(e.target.value);
+    console.log("hola handle");
+    console.log(tweetContent);
+  };
 
   const tweetear = (e) => {
-    e.preventDefault()
-    console.log('hola tweetear')
-    console.log(tweetContent)
-    handleClose()
-  }
+    e.preventDefault();
+    console.log("hola tweetear");
+    console.log(tweetContent);
+    handleClose();
+  };
 
   return (
     <>
-    <form onSubmit={tweetear}>
-      <input/>
-      <button></button>
-    </form>
-      {/* <Button className='btn btn-primary w-75 mt-5 rounded-5 fs-4' variant="primary" onClick={handleShow}>
-        <span className="fs-4 ms-1 d-none d-md-inline w-fit-content" style={{color: 'white'}}>Pigttear</span> 
-        <span className="d-md-none"><img width="50" src={logo} alt="cerdo" /> </span>
+      <form onSubmit={tweetear}>
+        <input />
+        <button></button>
+      </form>
+      <Button
+        className="btn btn-primary w-75 mt-5 rounded-5 fs-4"
+        variant="primary"
+        onClick={handleShow}
+      >
+        <span
+          className="fs-4 ms-1 d-none d-md-inline w-fit-content"
+          style={{ color: "white" }}
+        >
+          Pigttear
+        </span>
+        <span className="d-md-none">
+          <img width="50" src={logo} alt="cerdo" />{" "}
+        </span>
       </Button>
 
       <Modal show={show} onHide={handleClose}>
@@ -45,25 +56,28 @@ function PigComponent() {
             <Form.Group
               className="mb-3"
               controlId="exampleForm.ControlTextarea1"
-              
             >
               <Form.Label>¡Pigttea aquí!</Form.Label>
-              <Form.Control
-              onChange={handleTextChange}
-              value={tweetContent} 
-              />
+              <Form.Control onChange={handleTextChange} value={tweetContent} />
             </Form.Group>
+            <Button className="rounded-5" variant="primary" type="submit">
+              Pigttear
+            </Button>
+            <Button
+              className="rounded-5"
+              variant="secondary"
+              onClick={handleClose}
+            >
+              No Pigttear
+            </Button>
           </Form>
         </Modal.Body>
         <Modal.Footer>
-          <Button className='rounded-5' variant="secondary" onClick={handleClose}>
-            No Pigttear
-          </Button>
-          <Button className='rounded-5' variant="primary" type="submit">
+          {/* <Button className="rounded-5" variant="primary" type="submit">
             Pigttear
-          </Button>
+          </Button> */}
         </Modal.Footer>
-      </Modal> */}
+      </Modal>
     </>
   );
 }
