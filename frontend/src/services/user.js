e75d0d811d0a08bda7b9a88e39ff2a92260183e5
Cristina Garcia <gscristina112@gmail.com>
2022-09-09 08:52:37 +0200

    register error done

--- frontend/src/services/user.js
+++ frontend/src/services/user.js
@@ -1,9 +1,13 @@
-import axios from 'axios'
-const url = 'http://localhost:3001/api/users'
+import axios from "axios";
+const url = "http://localhost:3001/api/users";
 
-const register = async userRegisterInfo => {
-    const response = await axios.post(url, userRegisterInfo)
-    return response.data
-}
+const register = async (userRegisterInfo) => {
+  try {
+    const response = await axios.post(url, userRegisterInfo);
+    return response.data;
+  } catch {
+    alert("El usuario debe ser unico");
+  }
+};
 
-export default {register}
+export default { register };
