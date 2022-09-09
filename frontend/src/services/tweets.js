55b87892fbcf787933382293a4b3a2d59e0ea879
sergeisg <sergeisobera@gmail.com>
2022-09-09 09:41:04 +0200

    add get tweets service

--- frontend/src/services/tweets.js
+++ frontend/src/services/tweets.js
@@ -1,9 +1,14 @@
 import axios from 'axios'
 const url = 'http://localhost:3001/api/tweets'
 
+const getTweets = async () => {
+    const response = await axios.get(url)
+    return response.data
+}
+
 const postTweet = async tweetInfo => {
     const response = await axios.post(url, tweetInfo)
     return response.data
 }
 
-export default {postTweet}
+export default {getTweets, postTweet}
